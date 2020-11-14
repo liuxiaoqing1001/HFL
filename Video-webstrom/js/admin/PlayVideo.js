576cb7639fc7a9ca759ea76cf05ff37d0123a55d
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2020-11-14 11:31:45 +0800

    20201114更新2

--- Video-webstrom/js/admin/PlayVideo.js
+++ Video-webstrom/js/admin/PlayVideo.js
@@ -1,9 +1,9 @@
 $(function () {
-        $("showVideo").attr("src","http://localhost:8090/video/play/1")
-        // $("c").click(function () {
-        //         var vid="1";
-        //         var src= videoPath+vid;
-        //         document.getElementById("showVideo").src = src;
-        //         document.getElementById("showVideo").play();
-        // })
+        var id="/1/";
+        // // "http://localhost:8090/video/play/1"
+        var videoSrc= videoPlayPath + id;
+
+        $("#showVideo").attr("src",videoSrc);
+        $("#showVideo").play();
+
 });
