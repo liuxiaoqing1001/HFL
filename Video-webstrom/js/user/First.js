b2c21ede4043046a154f74f630f03c4807be5dfd
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2020-11-18 14:22:04 +0800

    20201118更新3

--- Video-webstrom/js/user/First.js
+++ Video-webstrom/js/user/First.js
@@ -6,5 +6,31 @@ $(function () {
 
     $('.carousel').carousel({
         interval: 2000
-    })
-})
+    });
+
+    $.get(
+        msgAllReceiver + "liu" ,
+        function(msgData) {
+            // console.log(msgData);
+            if(0 == msgData.errCode) {
+                msgArr = msgData.data ;
+                var str = '' ;
+                var i=0;
+                $.each(msgArr , function(index , item){
+                    str += '<li id="'+"li_"+i+'">'+
+                        '<span id="sender" class="msg-type">'+item.sender+'</span>'+
+                        '<span class="msg-title">'+item.title+'</span>'+
+                        '<span class="option">'+
+                        '<em class="data-time">'+item.time+'</em>'+
+                        '</span>'+
+                        '<div class="content">'+
+                        '<p class="msg">'+item.content+'</p></div></li>';
+                    //'<a id="'+item.id+'" class="glyphicon glyphicon-remove">删除</a>'+
+                    i++;
+                    // console.log(str);
+                });
+                $(".list").append(str) ;
+            }
+        }
+    );
+});
